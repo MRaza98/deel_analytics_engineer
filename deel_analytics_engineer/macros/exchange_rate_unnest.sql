f45f56f122036cdf20f4c7dd087ddefba48f2fc8
MRaza98 <raza.1998@hotmail.com>
2025-01-26 15:16:36 +0100

    using upto date json parser

--- deel_analytics_engineer/macros/exchange_rate_unnest.sql
+++ deel_analytics_engineer/macros/exchange_rate_unnest.sql
@@ -1,13 +1,13 @@
 {% macro exchange_rate_unnest(currency_code, column_name) %}
 
 CASE 
-    WHEN LOWER(currency) = 'eur' THEN CAST(JSON_EXTRACT_SCALAR({{column_name}}, '$.EUR') AS FLOAT64)
-    WHEN LOWER(currency) = 'cad' THEN CAST(JSON_EXTRACT_SCALAR({{column_name}}, '$.CAD') AS FLOAT64)
-    WHEN LOWER(currency) = 'mxn' THEN CAST(JSON_EXTRACT_SCALAR({{column_name}}, '$.MXN') AS FLOAT64)
-    WHEN LOWER(currency) = 'aud' THEN CAST(JSON_EXTRACT_SCALAR({{column_name}}, '$.AUD') AS FLOAT64)
-    WHEN LOWER(currency) = 'sgd' THEN CAST(JSON_EXTRACT_SCALAR({{column_name}}, '$.SGD') AS FLOAT64)
-    WHEN LOWER(currency) = 'usd' THEN CAST(JSON_EXTRACT_SCALAR({{column_name}}, '$.USD') AS FLOAT64)
-    WHEN LOWER(currency) = 'gbp' THEN CAST(JSON_EXTRACT_SCALAR({{column_name}}, '$.GBP') AS FLOAT64)
+    WHEN LOWER(currency) = 'eur' THEN CAST(JSON_VALUE({{column_name}}, '$.EUR') AS FLOAT64)
+    WHEN LOWER(currency) = 'cad' THEN CAST(JSON_VALUE({{column_name}}, '$.CAD') AS FLOAT64)
+    WHEN LOWER(currency) = 'mxn' THEN CAST(JSON_VALUE({{column_name}}, '$.MXN') AS FLOAT64)
+    WHEN LOWER(currency) = 'aud' THEN CAST(JSON_VALUE({{column_name}}, '$.AUD') AS FLOAT64)
+    WHEN LOWER(currency) = 'sgd' THEN CAST(JSON_VALUE({{column_name}}, '$.SGD') AS FLOAT64)
+    WHEN LOWER(currency) = 'usd' THEN CAST(JSON_VALUE({{column_name}}, '$.USD') AS FLOAT64)
+    WHEN LOWER(currency) = 'gbp' THEN CAST(JSON_VALUE({{column_name}}, '$.GBP') AS FLOAT64)
 
 END
 
